fe342c71d7f0089ae6da7602914db6edb33206ce
Andrei Glingeanu <andrei.glingeanu@gmail.com>
2016-11-10 19:15:52 +0200

    Update manifest.php

--- manifest.php
+++ manifest.php
@@ -11,3 +11,8 @@ $manifest['display'] = false;
 $manifest['standalone'] = true;
 
 $manifest['github_update'] = 'ThemeFuse/Unyson-Blog-Extension';
+
+$manifest['github_repo'] = 'https://github.com/ThemeFuse/Unyson-Blog-Extension';
+$manifest['uri'] = 'http://manual.unyson.io/en/latest/extension/introduction.html#content';
+$manifest['author'] = 'ThemeFuse';
+$manifest['author_uri'] = 'http://themefuse.com/';
