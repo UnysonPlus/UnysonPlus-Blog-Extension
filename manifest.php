df7473f3d03b2db8f593653d905f52fc267cbda1
jonmlas <jonmlas@gmail.com>
2025-08-21 18:51:06 +0800

    Manifest updated

--- manifest.php
+++ manifest.php
@@ -1,18 +1,26 @@
-<?php if ( ! defined( 'FW' ) ) {
-	die( 'Forbidden' );
-}
+<?php if (!defined('FW')) die('Forbidden');
 
 $manifest = array();
 
 $manifest['name']        = __( 'Blog Posts', 'fw' );
-$manifest['description'] = __( 'Blog Posts', 'fw' );
-$manifest['version'] = '1.0.2';
-$manifest['display'] = false;
-$manifest['standalone'] = true;
+$manifest['slug']        = 'unysonplus-blog-posts';
+$manifest['description'] = __( 'This extension adds Blog Posts functionality.', 'fw' );
 
-$manifest['github_update'] = 'ThemeFuse/Unyson-Blog-Extension';
+$manifest['version']     = '1.0.2';
+$manifest['display']     = false;
+$manifest['standalone']  = true;
 
-$manifest['github_repo'] = 'https://github.com/ThemeFuse/Unyson-Blog-Extension';
-$manifest['uri'] = 'http://manual.unyson.io/en/latest/extension/introduction.html#content';
-$manifest['author'] = 'ThemeFuse';
-$manifest['author_uri'] = 'http://themefuse.com/';
+// Repository Info
+$manifest['github_update'] = 'UnysonPlus/UnysonPlus-Blog-Extension';
+$manifest['github_repo']   = 'https://github.com/UnysonPlus/UnysonPlus-Blog-Extension';
+$manifest['github_branch'] = 'master';
+
+// Author Info
+$manifest['author']     = 'UnysonPlus';
+$manifest['author_uri'] = 'https://www.lastimosa.com.ph/unysonplus';
+
+// Meta
+$manifest['license']      = 'GPL-2.0-or-later';
+$manifest['text_domain']  = 'fw';
+$manifest['requires_php'] = '7.4';
+$manifest['requires_wp']  = '5.8';
